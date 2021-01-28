3fcbbdac094838d83914cea43c73895f37d91bac
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-01-28 13:18:19 -0600

    ran queries to test post code

--- picnic-fish.sql
+++ picnic-fish.sql
@@ -116,3 +116,25 @@ INSERT INTO Reactions ('label', 'image_url') VALUES ('happy', 'https://pngtree.c
 INSERT INTO Reactions ('label', 'image_url') VALUES ('heart', 'https://lh3.googleusercontent.com/proxy/BlwtWdiO1ucoroiKKuJN5CpiAUFA4tdHYRT_yXzxWLpNVTJS7UEVp1JV-lYshjAPeS7wd1pqXk6mpxY6rrSAPXD5NbBoE9hTf-1PpzofQbzNyH__1miggtO2IQKktovnAyPzjCW6T9mQG6JvgdHklZUaMd-YnIxeBPuP1lBw2E7fp9d6AR68');
 INSERT INTO AccountTypes ('label') VALUES ('Admin');
 INSERT INTO AccountTypes ('label') VALUES ('Author');
+
+INSERT INTO Posts (
+  "user_id",
+  "category_id",
+  "title",
+  "publication_date",
+  "image_url",
+  "content",
+  "approved"
+)
+VALUES (
+  1,
+  1,
+  "WonderDog",
+  1471300214792,
+  "https://wonderdogtraining.com/wp-content/uploads/2020/09/wonderdog-circle-logo-09.png",
+  "Wonder dog is a dog. He's wonderful.",
+  1
+);
+
+SELECT *
+FROM Posts
