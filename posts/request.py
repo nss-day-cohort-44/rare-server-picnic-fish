0ddbc48befd5226c4d49623ac115c100d9e8ccc0
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-01-29 15:02:52 -0600

    added get_single_post function

--- posts/request.py
+++ posts/request.py
@@ -3,14 +3,11 @@ import json
 from models import Post
 
 def get_all_posts():
-    # Open a connection to the database
-    with sqlite3.connect("./picnic-fish.db") as conn:
 
-        # Just use these. It's a Black Box.
+    with sqlite3.connect("./picnic-fish.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        # Write the SQL query to get the information you want
         db_cursor.execute("""
         SELECT
             p.id,
@@ -24,22 +21,37 @@ def get_all_posts():
         FROM posts p
         """)
 
-        # Initialize an empty list to hold all animal representations
         posts = []
 
-        # Convert rows of data into a Python list
         dataset = db_cursor.fetchall()
-
-        # Iterate list of data returned from database
         for row in dataset:
-
-            # Create an animal instance from the current row.
-            # Note that the database fields are specified in
-            # exact order of the parameters defined in the
-            # Animal class above.
-            post = Post(row['userId'], row['categoryId'], row["title"], row["publicationDate"], row['imageUrl'], row['content'], row['approved'])
-
+            post = Post(row['userId'], row['categoryId'], row["title"], row["publicationDate"],
+                            row['imageUrl'], row['content'], row['approved'])
             posts.append(post.__dict__)
 
-    # Use `json` package to properly serialize list as JSON
     return json.dumps(posts)
+
+def get_single_post():
+    with sqlite3.connect("./picnic-fish.db") as conn:
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            SELECT
+                SELECT
+            p.id,
+            p.user_id,
+            p.category_id,
+            p.title,
+            p.publication_date,
+            p.image_url,
+            p.content,
+            p.approved
+        FROM posts p
+            """, (id, ))
+
+            data = db_cursor.fetchone()
+
+            post = Post(data['userId'], data['categoryId'], data["title"], data["publicationDate"],
+                            data['imageUrl'], data['content'], data['approved'])
+            return json.dumps(post.__dict__)
