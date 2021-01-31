673bea48038c67add8df6ccb03c998c855a9958e
Knevels1 <kyle.a.nevels@icloud.com>
2021-01-31 16:11:49 -0600

    embedded the user and category onto the post seed data

--- posts/request.py
+++ posts/request.py
@@ -5,13 +5,17 @@ from models.user import User
 from models.categories import Category
 
 def get_all_posts():
-
+    # Open a connection to the database
     with sqlite3.connect("./picnic-fish.db") as conn:
+
+        # Just use these. It's a Black Box.
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
+        # Write the SQL query to get the information you want
         db_cursor.execute("""
         SELECT
+            p.id,
             p.user_id,
             p.category_id,
             p.title,
@@ -38,14 +42,32 @@ def get_all_posts():
             ON c.id = p.category_id
         """)
 
+        # Initialize an empty list to hold all animal representations
         posts = []
 
+        # Convert rows of data into a Python list
         dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
         for row in dataset:
-            post = Post(row['user_id'], row['category_id'], row['title'], row['publication_date'],
-                            row['image_url'], row['content'], row['approved'])
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Post class above.
+            post = Post(row['user_id'], row['category_id'], row["title"], row["publication_date"], row['image_url'], row['content'], row['approved'])
+
+            user = User(row['id'], row['first_name'], row['last_name'], row['email'],
+                        row['password'], row['bio'], row['username'], row['profile_image_url'], row['created_on'],
+                        row['active'], row['account_type_id'])
+            post.user = user.__dict__
+
+            category = Category(row['id'], row['label'])
+            post.category = category.__dict__
+
             posts.append(post.__dict__)
 
+    # Use `json` package to properly serialize list as JSON
     return json.dumps(posts)
 
 def get_single_post():
