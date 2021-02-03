0b4f89c6c4d61f13ef9798c72e2112e9d93eceed
Suganya <suganprakash0621@gmail.com>
2021-02-03 10:24:21 -0600

    coded for UPDATE

--- posts/request.py
+++ posts/request.py
@@ -107,8 +107,8 @@ def get_single_post(id):
 
             data = db_cursor.fetchone()
 
-            post = Post(data['user_id'], data['categoryId'], data["title"], 
-            data["publicationDate"], data['imageUrl'], data['content'], data['approved'], data['id'])
+            post = Post(data['user_id'], data['category_id'], data["title"], 
+            data["publication_date"], data['image_url'], data['content'], data['approved'], data['id'])
 
             user = User(data['id'], data['first_name'], data['last_name'], data['email'],
                         data['password'], data['bio'], data['username'], data['profile_image_url'], data['created_on'],
@@ -158,7 +158,7 @@ def update_post(id, new_post):
                 content=?,
                 approved=?
         WHERE id = ?
-        """, ((new_post['user_id'],new_post['categoryId'],new_post['title'],new_post['publicationDate'],
+        """, ((new_post['userId'],new_post['categoryId'],new_post['title'],new_post['publicationDate'],
               new_post['imageUrl'],new_post['content'],new_post['approved'], id,)))
 
         # Were any rows affected?
